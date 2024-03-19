61b0dec6f22547f0ba0abcec20555fe3f662f36e
Graham Keenan <graham.keenan@outlook.com>
2024-03-19 17:40:18 +0000

    added dns server abstraction

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,42 @@
 use std::net::UdpSocket;
 
-fn main() {
-    println!("Logs from your program will appear here!");
+#[derive(Debug)]
+struct DnsServer {
+    connection: UdpSocket,
+}
 
-    let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
-    let mut buf = [0; 512];
+impl DnsServer {
+    pub fn new(address: &str) -> Self {
+        let connection = UdpSocket::bind(address).expect("Failed to bind to address");
+        Self { connection }
+    }
 
-    loop {
-        match udp_socket.recv_from(&mut buf) {
-            Ok((size, source)) => {
-                println!("Received {} bytes from {}", size, source);
-                let response = [];
-                udp_socket
-                    .send_to(&response, source)
-                    .expect("Failed to send response");
-            }
-            Err(e) => {
-                eprintln!("Error receiving data: {}", e);
-                break;
+    pub fn listen(&self) {
+        let mut buffer = [0; 512];
+
+        loop {
+            match self.connection.recv_from(&mut buffer) {
+                Ok((size, source)) => {
+                    println!("Received {} bytes from {}", size, source);
+                    let response = [];
+                    self.connection
+                        .send_to(&response, source)
+                        .expect("Failed to send response");
+                }
+                Err(e) => {
+                    eprintln!("Error receiving data: {}", e);
+                    break;
+                }
             }
         }
     }
+}
+
+fn main() -> anyhow::Result<()> {
+    println!("Logs from your program will appear here!");
+
+    let dns_server = DnsServer::new("127.0.0.1:2053");
+    dns_server.listen();
+
+    Ok(())
 }
